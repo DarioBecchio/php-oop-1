1d3a8c2c7d6127ec43e78af11ad04fb5db2da31a
Dario Becchio <dabek1986@gmail.com>
2024-03-19 10:37:55 +0100

    create 2 methods

--- index.php
+++ index.php
@@ -6,31 +6,104 @@
   => all'interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 */ 
-
+//Modello è Movie
 class Movie {
-    public $genre;
-    public $length;
+    public $title;
+    public $duration;
+    public $cast;
+    public $overview;
     
-    /*public function setDurationLevel($length)
+    function __construct($title,$duration,$cast,$overview)
     {
-        if ($length > 100){
-            
-        }
-    };*/
+      //select an object property inside a class using the $this variable
+      //$this refers alwaysto an object
+      $this->title = $title;
+      $this->duration = $duration;
+      $this->cast = $cast;
+      $this->overview =$overview;
+    }
+    public function getCast()
+      //getter function
+      {
+          return $this->cast;
+      }
   
 
 };
-//echo Movie::$kindOf;
 
-$HarryPotter = new Movie();
-$GuardianOfGalaxy = new Movie();
+//Il modello movie ha dentro 2 istanze HarryPotter e Guardiani della galassia 
+$HarryPotter = new Movie("Harry Potter E il principe mezzosangue",120,[	
+    'Daniel Radcliffe',
+    'Rupert Grint',
+    'Emma Watson',
+    'Jim Broadbent',
+    'Helena Bonham Carter',
+    'Robbie Coltrane',
+    'Warwick Davis',
+    'Michael Gambon',
+    'Alan Rickman',
+    'Maggie Smith',
+    'Timothy Spall',
+    'David Thewlis',
+    'Julie Walters'],
+    "lorem ipsum dolor" );
+  
+$GuardianOfGalaxy = new Movie("Guardiani della galassia vol.2",100,['Chris Pratt',
+    'Zoe Saldaña',
+    'Dave Bautista',
+    'Vin Diesel',
+    'Bradley Cooper',
+    'Michael Rooker',
+    'Karen Gillan',
+    'Pom Klementieff',
+    'Sylvester Stallone',
+    'Kurt Russell'],"lorem ipsum dolor" );
 
 
-$HarryPotter->genre = "Fantasy"; 
-$HarryPotter->length = 120;
-$GuardianOfGalaxy->genre = "Action";
-$GuardianOfGalaxy->length = 100;
 
-var_dump($HarryPotter);
 
+
+    /*
+$HarryPotter->title = "Harry Potter E il principe mezzosangue"; 
+$HarryPotter->duration = 120;
+$HarryPotter->overview ="lorem ipsum dolor";
+$HarryPotter->cast=[	
+  'Daniel Radcliffe',
+  'Rupert Grint',
+  'Emma Watson',
+  'Jim Broadbent',
+  'Helena Bonham Carter',
+  'Robbie Coltrane',
+  'Warwick Davis',
+  'Michael Gambon',
+  'Alan Rickman',
+  'Maggie Smith',
+  'Timothy Spall',
+  'David Thewlis',
+  'Julie Walters'];
+$GuardianOfGalaxy->title = "Guardiani della galassia vol.2";  
+  $GuardianOfGalaxy->duration = 100;
+  $GuardianOfGalaxy->overview =" lorem ipsum dolor";
+  $GuardianOfGalaxy->cast = [	
+
+  'Chris Pratt',
+  'Zoe Saldaña',
+  'Dave Bautista',
+ 'Vin Diesel',
+  'Bradley Cooper',
+  'Michael Rooker',
+  'Karen Gillan',
+  'Pom Klementieff',
+  'Sylvester Stallone',
+  'Kurt Russell'];*/
+
+
+  //var_dump($HarryPotter , $GuardianOfGalaxy);
+
+
+
+var_dump($HarryPotter->getCast());
+var_dump($GuardianOfGalaxy->getCast());
+
+?>
 
